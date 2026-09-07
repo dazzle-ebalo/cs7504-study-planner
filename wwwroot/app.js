f313efb1884e46e9c435426e0caf137b74221f6c
dazzle-ebalo <dazzle.ebalo01@whitireianz.ac.nz>
2026-09-07 17:39:23 +1200

    add: Add element references and in-memory task array

---
+++ wwwroot/app.js
@@ -0,0 +1,8 @@
+const taskForm = document.querySelector("#task-form");
+const taskTitleInput = document.querySelector("#task-title");
+const taskDueInput = document.querySelector("#task-due");
+const taskList = document.querySelector("#task-list");
+const emptyMessage = document.querySelector("#empty-message");
+const taskCounter = document.querySelector("#task-counter");
+
+let tasks = [];
